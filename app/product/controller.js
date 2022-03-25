fc07909292b16297df7327541c4703d325c7a897
rianmfir <rianmfirdaus@gmail.com>
2022-03-25 11:31:52 +0700

    Relasi Product x Category x Tag

--- app/product/controller.js
+++ app/product/controller.js
@@ -2,11 +2,54 @@ const path = require('path');
 const fs = require('fs');
 const config = require('../config');
 const Product = require('./model');
-const { rootPath } = require('../config');
+const Category = require('../category/model');
+const Tag = require('../tag/model');
+
+const index = async (req, res, next) => {
+    try {
+        let { skip = 0, limit = 10 } = req.query;
+        let product = await Product
+            .find()
+            // Pagination
+            .skip(parseInt(skip))
+            .limit(parseInt(limit))
+            .populate('category')
+            .populate('tags');
+        return res.json(product);
+    } catch (err) {
+        next(err);
+    }
+}
 
 const store = async (req, res, next) => {
     try {
         let payload = req.body;
+
+        // relasi dengan category one-to-one
+        if (payload.category) {
+            let category =
+                await Category
+                    .findOne({ name: { $regex: payload.category, $options: 'i' } });
+
+            if (category) {
+                payload = { ...payload, category: category._id };
+            } else {
+                delete payload.category;
+            }
+        }
+
+        // relasi dengan tag one-to-many
+        if (payload.tags && payload.tags.length > 0) {
+            let tags =
+                await Tag.find({ name: { $in: payload.tags, $options: 'i' } });
+            if (tags.length) {
+                payload = { ...payload, tags: tags.map(tag => tag._id) };
+            } else {
+                delete payload.tags;
+            }
+        }
+
+
         if (req.file) {
             let tmp_path = req.file.path;
             let originalExt = req.file.originalname.split(".")[req.file.originalname.split(".").length - 1];
@@ -63,24 +106,35 @@ const store = async (req, res, next) => {
     }
 }
 
-const index = async (req, res, next) => {
-    try {
-        let { skip = 0, limit = 10 } = req.query;
-        let product = await Product
-            .find()
-            // Pagination
-            .skip(parseInt(skip))
-            .limit(parseInt(limit));
-        return res.json(product);
-    } catch (err) {
-        next(err);
-    }
-}
 
 const update = async (req, res, next) => {
     try {
         let payload = req.body;
         let { id } = req.params;
+
+        // relasi dengan category one-to-one
+        if (payload.category) {
+            let category =
+                await Category
+                    .findOne({ name: { $regex: payload.category, $options: 'i' } });
+
+            if (category) {
+                payload = { ...payload, category: category._id };
+            } else {
+                delete payload.category;
+            }
+        }
+
+        // relasi dengan tag one-to-many
+        if (payload.tags && payload.tags.length > 0) {
+            let tags =
+                await Tag.find({ name: { $in: payload.tags } });
+            if (tags.length) {
+                payload = { ...payload, tags: tags.map(tag => tag._id) };
+            } else {
+                delete payload.tags;
+            }
+        }
 
         if (req.file) {
             let tmp_path = req.file.path;
@@ -174,8 +228,8 @@ const destroy = async (req, res, next) => {
 }
 
 module.exports = {
-    store,
     index,
+    store,
     update,
     destroy
 }
--- app/product/model.js
+++ app/product/model.js
@@ -21,6 +21,16 @@ const productSchema = Schema({
 
     image_url: String,
 
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category'
+    },
+
+    tags: {
+        type: [Schema.Types.ObjectId],
+        ref: 'Tag'
+    }
+
 }, { timestamps: true });
 
 module.exports = model('Product', productSchema);
