16e733a1bb530955c2adc21dbe0bc47b856ad40c
rianmfir <rianmfirdaus@gmail.com>
2022-09-15 21:26:52 +0700

    Fix Bug Update Product

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { dbHost, dbPass, dbName, dbPort, dbUser } = require('../app/config');
 
-mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
-// mongoose.connect(`mongodb+srv://rianmfir:5YZSrg8OO6qZxAYc@food-store.kfld1gq.mongodb.net/foodstore?retryWrites=true&w=majority`);
+// mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
+mongoose.connect(`mongodb+srv://rianmfir:5YZSrg8OO6qZxAYc@food-store.kfld1gq.mongodb.net/foodstore?retryWrites=true&w=majority`);
 const db = mongoose.connection;
 module.exports = db;
